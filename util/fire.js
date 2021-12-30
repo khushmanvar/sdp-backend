9a7b3f469253f27b9801288e4513eeb4bd77de2e
khushmanvar <khushmanvar@gmail.com>
2021-12-30 10:02:47 +0530

    added firebase, heroku deploy config files

--- util/fire.js
+++ util/fire.js
@@ -1,14 +1,15 @@
 const firebase = require('firebase');
 
-var firebaseConfig = {
-    apiKey: "AIzaSyAlFDrNo46qmM8yUtKlrMDmSW7anNdbEQQ",
-    authDomain: "csi-website-cd583.firebaseapp.com",
-    projectId: "csi-website-cd583",
-    storageBucket: "csi-website-cd583.appspot.com",
-    messagingSenderId: "219220345469",
-    appId: "1:219220345469:web:42857fcd1627bd868e2714",
-    measurementId: "G-6H1ZJEQM6J"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyBX4O8ANgwVAPrAk029zAi0b6mY2ux3b0M",
+  authDomain: "easybus-43754.firebaseapp.com",
+  databaseURL: "https://easybus-43754-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "easybus-43754",
+  storageBucket: "easybus-43754.appspot.com",
+  messagingSenderId: "1087453155493",
+  appId: "1:1087453155493:web:64645ff463b3403f897d4c",
+  measurementId: "G-QKKCYZZQBP"
+};
 
 firebase.initializeApp(firebaseConfig);
 

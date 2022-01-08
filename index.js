deb583de816af108811cb85dd92486a41c08f5d4
khushmanvar <khushmanvar@gmail.com>
2022-01-08 12:36:46 +0530

    add conductor routes

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000
-const { login } = require("./handlers/user")
+const { conductLogin } = require("./handlers/conductor")
 
 // express middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // admin routes
-app.post('/admin  /login', login);
+app.post('/admin/login', conductLogin);
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Easybus!')

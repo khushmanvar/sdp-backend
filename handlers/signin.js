1e57e588c85b4818cecafd013acb254631356015
khushmanvar <khushmanvar@gmail.com>
2022-01-14 12:21:31 +0530

    added token in response

--- handlers/signin.js
+++ handlers/signin.js
@@ -5,8 +5,14 @@ exports.signin = (req, res) => {
     firebase
     .auth()
     .signInWithEmailAndPassword(req.body.email, req.body.password)
-  .then((userCredential) => {
-    db.collection("Users").doc(userCredential.user.uid).get().then((user) => {
+    .then((data) => {
+      return data.user.getIdToken();
+    })
+    .then((token) => {
+      return res.json({ token });
+    })
+    .then((userCredential) => {
+      db.collection("Users").doc(userCredential.user.uid).get().then((user) => {
         return res.status(200).json({"email": user.data().email, "name": user.data().name, "phoneNo": user.data().phoneNo});
     });
   })

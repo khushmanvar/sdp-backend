02d134286d74c137c2ab06824d92c5b119d13bbf
khushmanvar <khushmanvar@gmail.com>
2022-01-14 13:41:03 +0530

    added headers for cors

--- index.js
+++ index.js
@@ -13,11 +13,6 @@ const {userDetails} = require("./handlers/userDetails");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
-app.use(function(req, res, next) {
-  res.set("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 // admin routes
 app.get('/admin/:cid', getConductorDetails);

73e039a210683c1f988b17a9abd66346f99e05e2
khushmanvar <khushmanvar@gmail.com>
2022-01-17 10:48:17 +0530

    added realtime database config

--- util/admin.js
+++ util/admin.js
@@ -3,7 +3,8 @@ const admin = require('firebase-admin');
 const serviceAccount = require('../easybus-43754-firebase-adminsdk-ryy5l-c062abdfb5.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://easybus-43754-default-rtdb.asia-southeast1.firebasedatabase.app"
 });
 
 const db = admin.firestore();

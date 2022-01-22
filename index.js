bb6b604fc602f4398270e82f37413c66db4eff7a
khushmanvar <khushmanvar@gmail.com>
2022-01-22 20:11:41 +0530

    added headers for cors

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); 
-  res.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
